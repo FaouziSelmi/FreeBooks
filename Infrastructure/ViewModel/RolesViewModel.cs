8ccaca93a3165d95453e60dcf2a12d18d34b10f0
FaouziSelmi <selmifaouzi@gmail.com>
2022-08-12 01:22:39 +0100

    add code

---
+++ Infrastructure/ViewModel/RolesViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.ViewModel
+{
+    public class RolesViewModel
+    {
+        public List<IdentityRole> Roles { get; set; }
+        public NewRole NewRole { get; set; }
+    }
+    public class NewRole
+    {
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+    }
+}
--- WebBook/Areas/Admin/Controllers/AccountsController.cs
+++ WebBook/Areas/Admin/Controllers/AccountsController.cs
@@ -1,10 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Infrastructure.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebBook.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class AccountsController : Controller
     {
-        [Area("Admin")]
+        private readonly RoleManager<IdentityRole> _RoleManager;
+
+        public AccountsController(RoleManager<IdentityRole> roleManager)
+        {
+            _RoleManager = roleManager;
+        }
+        public IActionResult Roles()
+        {
+            var Model = new RolesViewModel 
+            {
+             NewRole = new NewRole(),
+             Roles= _RoleManager.Roles.OrderBy(x=>x.Name).ToList()
+            };
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public  async Task <IActionResult> Roles(RolesViewModel Model)
+        {
+            if (ModelState.IsValid)
+            {
+                var role = new IdentityRole
+                {
+                    Id=Model.NewRole.RoleId,
+                    Name=Model.NewRole.RoleName
+                };
+                //Create
+                if(role.Id==null)
+                {
+                    role.Id = Guid.NewGuid().ToString();
+                    var result=  await _RoleManager.CreateAsync(role);
+                    if (result.Succeeded)
+                    {
+                        //Succeeded
+                    }
+                    else
+                    { // not Succeeded 
+                    }
+                }
+                //Update
+                else 
+                {
+                }
+            }
+            return View();
+        }
         public IActionResult Login()
         {
             return View();
